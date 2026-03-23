b18538a285ad7ddc219141f757453e49e8935259
quangdung2004 <dungnqhe186457@fpt.edu.vn>
2026-03-24 04:38:39 +0700

    remove hardcoded SQL Server connection from DbContext

--- backend/CloneEbay.Infrastructure/Persistence/CloneEbayDbContext.cs
+++ backend/CloneEbay.Infrastructure/Persistence/CloneEbayDbContext.cs
@@ -48,9 +48,7 @@ public partial class CloneEbayDbContext : DbContext
     public virtual DbSet<ShippingWebhookEvent> ShippingWebhookEvent { get; set; }
 
     public virtual DbSet<OrderAddressChangeHistory> OrderAddressChangeHistory { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=CloneEbayDB;User Id=sa;Password=123;TrustServerCertificate=True;");
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
